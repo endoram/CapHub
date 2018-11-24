422a45ecbc67a753cf5e9010c39648413a0e2995
Unknown <spencer.mcconnell2003@gmail.com>
2018-11-23 19:52:56 -0700

    Added db connection fail catch Corrected validation to from

--- dashboard/RMR-ID-073/index.php
+++ dashboard/RMR-ID-073/index.php
@@ -1,40 +1,43 @@
 <?php
-require "../header.php";
 require "config_m.php";
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $capid = $_POST['capid'];
   $password = $_POST['password'];
-
   if(!is_numeric($capid)) {
     $errorMsg = "Invalid Cap ID or password";
   }
   else {
     $conn = new mysqli($mysql_host, $mysql_user, $mysql_password, $mysql_database) or die("Database Connection Failed : " . mysql_error());
 
-    $query = "SELECT user_pass FROM sq_members WHERE cap_id=" . $capid;
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+
+    $query = "SELECT * FROM sq_members WHERE cap_id=$capid";
     $result = $conn->query($query);
 
     if ($result->num_rows > 0) {
       while($row = $result->fetch_assoc()) {
-          $db_pass = $row["password"];
+          $db_pass = $row['user_pass'];
       }
       if (strcmp($db_pas,$password)) {
-        header("Location: main.php");
+        $conn->close();
+        header("Location: https://caphub.org/RMR-ID-073/main.php");
+        exit();
       }
       else {$errorMsg = "Invalid Cap ID or password";}
       $conn->close();
     }
     else {
-      echo "<p style=\"color: red;\">*Invalid Cap ID or password</p>\n\n";
-        $conn->close();
+      $errorMsg = "Invalid Cap ID or password";
+      $conn->close();
     }
   }
 }
-if(isset($errorMsg) && $errorMsg) {
-  echo "<p style=\"color: red;\">*",htmlspecialchars($errorMsg),"</p>\n\n";
-}
+?>
 
+<?php
+require "../header.php";
 ?>
 
 <html>
@@ -44,6 +47,10 @@ if(isset($errorMsg) && $errorMsg) {
   <body>
     <div class="loginform">
       <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" accept-charset="UTF-8">
+        <?php
+        if(isset($errorMsg) && $errorMsg) {
+          echo "<p style=\"color: red;\">*",htmlspecialchars($errorMsg),"</p>\n\n";
+        }?>
         <label for="EMIAL">cap ID:</label> <input type="text" name="capid" align="right" value="<?PHP if(isset($_POST['capid'])) echo htmlspecialchars($_POST['capid']); ?>"><br>
         <label for="PASSWORD">Password:</label> <input type="password" name="password" align="right" value="<?PHP if(isset($_POST['password'])) echo htmlspecialchars($_POST['password']); ?>"><br>
         <input type="submit" value="Login">
