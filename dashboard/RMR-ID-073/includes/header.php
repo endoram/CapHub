99ea33fe97fb6c4b58f785260c05a9743ff6b7da
Unknown <spencer.mcconnell2003@gmail.com>
2018-11-25 23:07:29 -0700

    Added table with links

--- dashboard/RMR-ID-073/includes/header.php
+++ dashboard/RMR-ID-073/includes/header.php
@@ -1,4 +1,38 @@
 <?php
-echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
-echo '<img src="../images/banner.png">';
+  require "includes/control_access.php";
+
+  if(isset($_GET['logout'])) {
+    header("Location: index.php");
+  }
+
+  echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+  echo '<img src="images/banner.png">';
 ?>
+
+
+<style>
+table {
+  text-align: center;
+  border: 1px solid black;
+}
+
+th, td {
+  border: 1px solid black;
+  padding: 5px;
+  text-align: left;
+}
+</style>
+
+<html>
+  <body>
+    <table style="width:50%">
+        <tr>
+          <td><a href="coms.php">Comunications</a></td>
+          <td><a href="events.php">Events</a></td>
+          <td><a href="sqmembers.php">Squadron Members</a></td>
+          <td><a href="help.php">Help</a></td>
+          <td><a href="?logout=1">Log out</a></td>
+        </tr>
+    </table>
+  </body>
+</html>
