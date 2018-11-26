513fa19b4eaa8dffe5dce16e7bbadb972807490c
Unknown <spencer.mcconnell2003@gmail.com>
2018-11-25 23:07:00 -0700

    All files are clickable links

---
+++ dashboard/RMR-ID-073/help.php
@@ -0,0 +1,16 @@
+<?php
+require "includes/header.php";
+?>
+
+<html>
+  <head>
+    <title>CapHub MainPage</title>
+  </head>
+  <body>
+    <h3>If you have any questions, want to report a bug or request things to be added</h3>
+    <br>
+    <h3>Be sure to send the creaters an email at</h3>
+    <br>
+    <p>caphub.org@gmail.com</p>
+  </body>
+</html>
