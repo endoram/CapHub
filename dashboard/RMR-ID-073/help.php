18e9909dc219a85f34c6f2f3fbbad931467499fe
Unknown <spencer.mcconnell2003@gmail.com>
2018-11-26 17:04:29 -0700

    Fixed spelling errors

--- dashboard/RMR-ID-073/help.php
+++ dashboard/RMR-ID-073/help.php
@@ -7,10 +7,12 @@ require "includes/header.php";
     <title>CapHub MainPage</title>
   </head>
   <body>
+    <center>
     <h3>If you have any questions, want to report a bug or request things to be added</h3>
     <br>
-    <h3>Be sure to send the creaters an email at</h3>
+    <h3>Be sure to send the creators an email at:</h3>
     <br>
     <p>caphub.org@gmail.com</p>
+  </center>
   </body>
 </html>
