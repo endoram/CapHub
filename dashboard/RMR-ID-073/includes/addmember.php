9024a4adaae5bdcf8d96c05037088956748be945
Unknown <spencer.mcconnell2003@gmail.com>
2018-12-06 17:04:43 -0700

    Added check for duplicate capids

--- dashboard/RMR-ID-073/includes/addmember.php
+++ dashboard/RMR-ID-073/includes/addmember.php
@@ -20,32 +20,50 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $x = 1;
   }
   if ($cadetornot == ""){
-    $errorMsg = "Invalid cadet or senior optionasdfsadfad";
+    $errorMsg = "Invalid cadet or senior option";
     $x = 1;
   }
   if (!in_array($cadetornot, array('cadet','senior', 'Cadet', 'Senior'), true )) {
     $errorMsg = "Invalid cadet or senior option";
     $x = 1;
   }
-  if ($x == 0) {adduser($firstname, $lastname, $capid, $cadetornot);}
+  if ($x == 0) {adduser($firstname, $lastname, $capid, $cadetornot, $errorMsg);}
 }
 
-function adduser($firstname, $lastname, $capid, $cadetornot) {
+function adduser($firstname, $lastname, $capid, $cadetornot, $errorMsg) {
   require "config_m.php";
 
-  $query = "INSERT INTO sq_members (cap_id, first_name, last_name, cadet_senior) VALUES (" . $capid . ",'" . $firstname . "', '" . $lastname . "', '" . $cadetornot . "')";
-  $conn->query($query);
+  $y = 0;
+
+  $query = "SELECT cap_id FROM sq_members WHERE cap_id=" . $capid;
+  $result = $conn->query($query);
+
+  if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      if ($row['cap_id'] == $capid) {
+        $GLOBALS['errorMsg'] = "A member already is asigned that CAP ID";
+        $y = 1;
+      }
+    }
+  }
+
+  if ($y == 0) {
+    $query = "INSERT INTO sq_members (cap_id, first_name, last_name, cadet_senior) VALUES (" . $capid . ",'" . $firstname . "', '" . $lastname . "', '" . $cadetornot . "')";
+    $conn->query($query);
+    $conn->close();
+    header("Location: ../protected/sqmembers.php");
+  }
   $conn->close();
-  header("Location: ../protected/sqmembers.php");
 }
 
-require "header.php";
 require "config_m.php";
 ?>
 
 
 <html>
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <img src="../images/banner.png">
     <link rel="stylesheet" type="text/css" href="../protected/style.css">
     <title>CapHub Add Member</title>
   </head>
@@ -63,8 +81,10 @@ require "config_m.php";
         <input type="submit" value="Add Member">
       </form>
     </div>
-    <form action="../protected/main.php">
-      <input type="submit" value="Cancel" />
-    </form>
+    <div class="cancelbutton">
+      <form action="../protected/sqmembers.php">
+        <input type="submit" value="Cancel">
+      </form>
+    </div>
   </body>
 </html>
