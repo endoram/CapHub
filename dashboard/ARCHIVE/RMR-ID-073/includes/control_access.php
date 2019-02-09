5819bb47de2dd02971b6b16468b5f66e2170bf8e
Unknown <spencer.mcconnell2003@gmail.com>
2019-02-08 21:13:02 -0700

    Removed css, header jpg, and archive

--- dashboard/ARCHIVE/RMR-ID-073/includes/control_access.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-if(!isset($_SESSION['password'])){
-  header("Location: ../index.php");
-  exit();
-}
-?>
--- dashboard/ARCHIVE/header.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
-?>
