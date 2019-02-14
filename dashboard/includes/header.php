fe3f4f8dc566ccd68926e14bd2bb9a2d2665ebea
Unknown <spencer.mcconnell2003@gmail.com>
2019-02-13 22:10:55 -0700

    Added separate views for different privlages

--- dashboard/includes/header.php
+++ dashboard/includes/header.php
@@ -1,5 +1,10 @@
 <?php
+  session_start();
   require "control_access.php";
+
+  if(isset($_GET['logout'])) {
+    header("Location: ../index.php");
+  }
 ?>
 
 <html>
@@ -11,10 +16,18 @@
   <body>
     <div class="menubar">
       <ul>
-        <li><a href="coms.php">Radios</a></li>
-        <li><a href="events.php">Events</a></li>
+        <?php if($_SESSION['privlv'] >= 2){ //Allows Seniors and Seniors that need cadet stuff ?>
+          <li><a href="coms.php">Radios</a></li>
+          <li><a href="events.php">Events</a></li>
+        <?php } ?>
         <li><a href="sqmembers.php">Squadron</a></li>
         <li><a href="Meeting_nights.php">Meetings</a></li>
+        <?php if($_SESSION['privlv'] <= 1 || $_SESSION['privlv'] == 2 || $_SESSION['privlv'] == 4){ //Only shows up for cadets and Senior working with cadets?>
+          <li><a href="physical_testing.php">PT</a></li>
+        <?php } ?>
+        <?php if($_SESSION['privlv'] >= 3){ ?>
+          <li><a href="vehicles.php">Vehicles</a></li>
+        <?php } ?>
         <li><a href="help.php">Help</a></li>
         <li><a href="?logout=1">Log out</a></li>
       </ul>
