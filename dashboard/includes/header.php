d486c49ca94541d698634b4535293d3582a614bb
Unknown <spencer.mcconnell2003@gmail.com>
2019-03-03 18:37:13 -0700

    Added view of radios, can add and remove radios

--- dashboard/includes/header.php
+++ dashboard/includes/header.php
@@ -16,17 +16,11 @@
   <body>
     <div class="menubar">
       <ul>
-        <?php if($_SESSION['privlv'] >= 2){ //Allows Seniors and Seniors that need cadet stuff ?>
-          <li><a href="coms.php">Radios</a></li>
-          <li><a href="events.php">Events</a></li>
-        <?php } ?>
+        <li><a href="coms.php">Coms</a></li>
         <li><a href="sqmembers.php">Squadron</a></li>
         <li><a href="meeting_nights.php">Meetings</a></li>
-        <?php if($_SESSION['privlv'] <= 2 || $_SESSION['privlv'] == 4){ //Only shows up for cadets and Senior working with cadets?>
-          <li><a href="physical_testing.php">PT</a></li>
-        <?php } ?>
+        <li><a href="physical_testing.php">PT</a></li>
         <?php if($_SESSION['privlv'] >= 2){ ?>
-          <li><a href="vehicles.php">Vehicles</a></li>
         <?php } ?>
         <li><a href="help.php">Help</a></li>
         <li><a href="?logout=1">Log out</a></li>
@@ -35,17 +29,11 @@
     <div class="dropdownheader">
       <button class="dropbtn">Menu</button>
       <div class="dropdown-content">
-        <?php if($_SESSION['privlv'] >= 2){ //Allows Seniors and Seniors that need cadet stuff ?>
-          <a href="coms.php">Radios</a>
-          <a href="events.php">Events</a>
-        <?php } ?>
+        <a href="coms.php">Coms</a>
         <a href="sqmembers.php">Squadron</a>
         <a href="meeting_nights.php">Meetings</a>
-        <?php if($_SESSION['privlv'] <= 2 || $_SESSION['privlv'] == 4){ //Only shows up for cadets and Senior working with cadets?>
-          <a href="physical_testing.php">PT</a>
-        <?php } ?>
+        <a href="physical_testing.php">PT</a>
         <?php if($_SESSION['privlv'] >= 2){ ?>
-          <a href="vehicles.php">Vehicles</a>
         <?php } ?>
         <a href="help.php">Help</a>
         <a href="?logout=1">Log out</a>
