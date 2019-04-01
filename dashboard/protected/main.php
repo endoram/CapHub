331864a41d992bf18a660e323e3712a4454d907a
Unknown <spencer.mcconnell2003@gmail.com>
2019-03-31 20:13:15 -0600

    Updated version number to 1.2

--- dashboard/protected/main.php
+++ dashboard/protected/main.php
@@ -13,12 +13,19 @@ require "../includes/header.php";
       <?php echo "Privlage Level: " . $_SESSION['privlv'];?>
     </div>
     <div class="news">
-      <h3>1.1:</h3>
+      <h3>1.2:</h3>
       <div class="lists">
         <dl>
+          <dt><b>Whats New:</b></dt>
+            <ul>
+              <li>Date selector for seaching by date</li>
+              <li>A way to retire squadron members</li>
+            </ul>
+          <br>
+          <br>
           <dt><b>Squadron page:</b></dt>
-            <dd>Allows you to add members, remove members and search members.</dd>
-            <dd>There is a new table design with sorting capabilities.</dd>
+            <dd>Allows you to add members, remove members, search members</dd>
+            <dd>and retire members.</dd>
             <br>
           <dt><b>Meetings page:</b></dt>
             <dd>Allows cadets to sign themselves in and out of meetings.</dd>
@@ -27,6 +34,9 @@ require "../includes/header.php";
           <dt><b>PT page:</b></dt>
             <dd>Members can now enter PT data without transfer of paper.</dd>
             <dd>Logs are searchable by name, CAP ID and date.</dd>
+          <dt><b>Comms page:</b></dt>
+            <dd>keep track of radios that are checked out or in.</dd>
+            <dd>Lets you know who has check them out.</dd>
         </dl>
         <br>
         <b>Top Navigation Bar:</b>
