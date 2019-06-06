22d443f1503ef6e9dbd7841372673a30cc2b364f
Unknown <spencer.mcconnell2003@gmail.com>
2019-06-05 18:40:24 -0600

    Added radio status, color coded, removed Radio name

--- dashboard/protected/comms.php
+++ dashboard/protected/comms.php
@@ -5,18 +5,16 @@ if(isset($_POST['sent'])) {
   if($_POST['sent'] == "Radio ID:") {
     $radio_id = $_POST['input'];
     $radio_type = $_POST['radio_type'];
-    $radio_name = $_POST['radio_name'];
 
     require "../includes/config_m.php";
     $query = "SELECT * FROM comms WHERE radio_id='$radio_id'";
     $result = $conn->query($query);
+
     if ($result->num_rows > 0) {$errorMsg = "A radio with that ID has already been added"; $conn->close();}
     else {
-      if(isset($_POST['radio_name'])){
-        $query = "INSERT INTO comms (radio_id, radio_name, radio_type, status) VALUES ('$radio_id','$radio_name', '$radio_type', 'IN')";
-        $conn->query($query);
-        $conn->close();
-      }
+      $query = "INSERT INTO comms (radio_id, radio_type, in_out, status) VALUES ('$radio_id', '$radio_type', 'IN', 'Fully Operational')";
+      $conn->query($query);
+      $conn->close();
     }
   }
   if($_POST['sent'] == "Remove Radio ID:") {
@@ -49,14 +47,23 @@ if(isset($_POST['sent'])) {
       $name = $firstname . " " . $lastname;
     }
 
-    $query = "UPDATE comms SET status='OUT', name='$name' WHERE radio_id='$radio_id'";
+    $query = "UPDATE comms SET in_out='OUT', name='$name' WHERE radio_id='$radio_id'";
     $conn->query($query);$conn->close();
   }
   if($_POST['sent'] == "CheckIn Radio ID:") {
     require "../includes/config_m.php";
     $radio_id = $_POST['input'];
-    $query = "UPDATE comms SET status='IN', name='' WHERE radio_id='$radio_id'";
+    $query = "UPDATE comms SET in_out='IN', name='' WHERE radio_id='$radio_id'";
 
+    $conn->query($query);$conn->close();
+  }
+  if($_POST['sent'] == "Radio ID: ") {
+    require "../includes/config_m.php";
+    $radio_id = $_POST['input'];
+    $whatsbroken = $_POST["whatbroken"];
+    $status = $_POST['change_status'];
+
+    $query = "UPDATE comms SET status='$status' WHERE radio_id='$radio_id'";
     $conn->query($query);$conn->close();
   }
 }
@@ -65,6 +72,7 @@ if(isset($_GET['addradio'])) {$data = "Radio ID:";  handleit($data);}
 if(isset($_GET['removeradio'])) {$data = "Remove Radio ID:";  handleit($data);}
 if(isset($_GET['checkout'])) {$data = "CheckOut Radio ID:"; handleit($data);}
 if(isset($_GET['checkin'])) {$data = "CheckIn Radio ID:"; handleit($data);}
+if(isset($_GET['changestatus'])) {$data = "Radio ID: "; handleit($data);}
 
 function handleit($data) {
   unset($_GET['firstname, lastname, capid']);
@@ -80,17 +88,29 @@ function handleit($data) {
     echo '<input type="text" name="capid" required>';
   }
   if(isset($_GET['addradio'])) {
-    echo '<label for="input"><b>Radio Name:</b></label>';
-    echo '<input type="text" name="radio_name" required>';
+  //  echo '<label for="input"><b>Radio Name:</b></label>';
+  //  echo '<input type="text" name="radio_name" required>';
+    echo '
+      <select name="radio_type">
+        <option value=ISR>ISR</option>
+        <option value=VHF>VHF</option>
+        <option value=HF>HF</option>
+      </select>
+    ';
   }
 
-  echo '
-    <select name="radio_type">
-      <option value=ISR>ISR</option>
-      <option value=VHF>VHF</option>
-      <option value=HF>HF</option>
-    </select>
-  ';
+  if(isset($_GET['changestatus'])){
+    echo '<label for="input"><b>Whats Broken:</b></label>';
+    echo '<input type="text" name="whatbroken" required>';
+    echo '
+    <select name="change_status">
+      <option value=Fully Operational>Fully Operational</option>
+      <option value=Operational>Operational</option>
+      <option value=Broken>Broken</option>
+      <option value=Batteries>Out of Batteries</option>
+    </select> ';
+  }
+
   echo '<button type="submit" value="' . $data . '" name="sent" class="btn">Submit</button>';
   echo '<button type="button" class="btn cancel" onclick="closeForm()">Close</button>';
   echo '</form>';
@@ -123,6 +143,7 @@ function closeForm() {
             <li><a href="?removeradio">Remove Radio</a><li>
             <li><a href="?checkout">CheckOut Radio</a><li>
             <li><a href="?checkin">CheckIn Radio</a><li>
+            <li><a href="?changestatus">Change Radio Status</a><li>
           </ul>
         </div>
       </div>
@@ -135,33 +156,41 @@ function closeForm() {
         <div class="radiotable">
           <br>
           <?php
-          $table = array("SELECT * FROM comms WHERE status='OUT'","SELECT * FROM comms WHERE radio_type='ISR'", "SELECT * FROM comms WHERE radio_type='VHF'", "SELECT * FROM comms WHERE radio_type='HF'");
+          $table = array("SELECT * FROM comms WHERE in_out='OUT'","SELECT * FROM comms WHERE radio_type='ISR'", "SELECT * FROM comms WHERE radio_type='VHF'", "SELECT * FROM comms WHERE radio_type='HF'");
 
           foreach ($table as $key => $value) {
-            echo '
-            <br>
-              <table>
-                <colgroup>
-                  <col span="5" style="background-color:lightgrey">
-                </colgroup>
-                <tr>
-                  <th>Radio ID</th>
-                  <th>Radio Name</th>
-                  <th>Type</th>
-                  <th>Status</th>
-                  <th>Name</th>
-                </tr>
-            ';
             require "../includes/config_m.php";
             $result = $conn->query($value);
-
+              //<th>Radio Name</th>
+              //<td>" . $row["radio_name"] . "</td>
             if ($result->num_rows > 0) {
+              echo '
+              <br>
+                <table>
+                  <colgroup>
+                    <col span="5" style="background-color:lightgrey">
+                  </colgroup>
+                  <tr>
+                    <th>Radio ID</th>
+                    <th>Type</th>
+                    <th>Status</th>
+                    <th>In/Out</th>
+                    <th>Name</th>
+                  </tr>
+              ';
               while($row = $result->fetch_assoc()) {
                 echo "<tr>
                 <td>" . $row["radio_id"] . "</td>
-                <td>" . $row["radio_name"] . "</td>
-                <td>" . $row["radio_type"] . "</td>
-                <td>" . $row["status"] . "</td>
+                <td>" . $row["radio_type"] . "</td>";
+              //  <td>" . $row["status"] . "</td>
+              if($row["status"] == "Fully Operational") {echo '<td bgcolor="#00FF00">' . $row["status"] . "</td>";}
+              else {if($row["status"] == "Operational") {echo "<td bgcolor='#FFFF00'>" . $row["status"] . "</td>";}
+                else{  if($row["status"] == "Broken") {echo "<td bgcolor='#FF0000'>" . $row["status"] . "</td>";}
+                  if($row["status"] == "Batteries") {echo "<td bgcolor='#000000'>" . $row["status"] . "</td>";}
+                }
+              }
+              echo "
+                <td>" . $row["in_out"] . "</td>
                 <td>" . $row["name"] . "</td>
                 </tr>";
               }
