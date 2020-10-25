dd00be5aa14584bfe3355188c47d897f3add929f
Spencer McConnell <spencer.mcconnell2003@gmail.com>
2020-10-25 16:00:35 -0600

    Added export to CSV

---
+++ dashboard/includes/export.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=data.csv');
+
+$output = fopen('php://output', 'w');
+
+// output the column headings
+fputcsv($output, array('First Name', 'Last Name', 'CAP ID'));
+
+if(isset($_SESSION['query_idea'])) {
+  $query = $_SESSION['query_idea'];
+  unset($_SESSION['query_idea']);
+}
+
+require "../includes/config_m.php";
+$result = $conn->query($query);
+
+// loop over the rows, outputting them
+while($row = $result->fetch_assoc()) {
+  fputcsv($output, $row);
+}
+exit()
+?>
