4bd4d575770efc879517c18a529dc7662d10b1a4
Spencer McConnell <spencer.mcconnell2003@gmail.com>
2022-08-17 16:14:07 -0600

    ISSUE - Exports all columns :|

--- dashboard/includes/export.php
+++ dashboard/includes/export.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 header('Content-Type: text/csv; charset=utf-8');
+#Make title of csv date that was queryed
 header('Content-Disposition: attachment; filename=data.csv');
 
 $output = fopen('php://output', 'w');
@@ -18,7 +19,13 @@ $result = $conn->query($query);
 
 // loop over the rows, outputting them
 while($row = $result->fetch_assoc()) {
-  fputcsv($output, $row);
+	fputcsv($output, $row);
+ # fputcsv($output, $row["date"]);
+ # fputcsv($output, $row["cap_id"]);			#fputcsv REQUIRES an ARRAY - Will come back
+ # fputcsv($output, $row["name"]);
+ # fputcsv($output, $row["time_in"]);
+ # fputcsv($output, $row["time_out"]);
+ # fputcsv($output, $row["member_type"]);
 }
 unset($_SESSION['query_idea']);
 exit()
